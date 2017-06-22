7e49950111713d044e616f6f2ef769be70fff8a0
Evan Gatchell <greygatch@gmail.com>
2017-06-22 18:09:27 -0500

    Update index.js
    Fix solves following linting error: 'A constructor name should not start with a lowercase letter'.
    
    http://eslint.org/docs/rules/new-cap

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var PayFlowGateway = require('./lib/PayFlowGateway.js');
 var PayFlowReport = require('./lib/PayFlowReport.js');
 
 module.exports = {
-  gateway: function factory(conf) {
+  Gateway: function factory(conf) {
     return new PayFlowGateway(conf);
   },
   report: {
